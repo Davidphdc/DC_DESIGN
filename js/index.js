23ce880d351ab54c7b8c34263cf87ce0379f84f0
David Cvetkovic <dc@davidcvetkovic.com>
2024-01-25 15:36:15 +1100

    New Build Test

---
+++ js/index.js
@@ -0,0 +1,36 @@
+
+// Menu overlay close btn
+const nav = document.querySelector(".nav");
+const menuBtn = document.querySelector(".menu-btn");
+const closeBtn = document.querySelector(".close-btn");
+const hidenav = document.getElementById("hidenav");
+
+menuBtn.addEventListener("click", () => {
+  nav.classList.add("active");
+  menuBtn.classList.add("displaynone");
+});
+
+closeBtn.addEventListener("click", () => {
+  nav.classList.remove("active");
+  menuBtn.classList.remove("displaynone");
+});
+
+hidenav.addEventListener("click", () => {
+  hidenav.classList.remove("active");
+  menuBtn.classList.remove("displaynone");
+});
+
+
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('.nav_link').forEach(link => {
+  if(link.href.includes(`${activePage}`)){
+    link.classList.add('active');
+  
+  }
+});
+
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
